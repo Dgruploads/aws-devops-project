9cedbe40085983aed1d449b3e5d89a66674120ce
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2021-03-12 17:24:55 +0530

    Add files via upload

---
+++ scripts/copy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+################################################################################################################
+# Please note this is an example copy path. You will need to change this according to the path on  your server.#
+################################################################################################################
+cp -R /opt/codedeploy-agent/deployment-root/*/*/deployment-archive/*.war /usr/local/tomcat/webapps/
