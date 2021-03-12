dd3a06cbff02e9a09ed8dcbd2803db35d3062162
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2021-03-12 20:09:32 +0530

    Update copy.sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -3,10 +3,10 @@
 # Please note this is an example copy path. You will need to change this according to the path on  your server.#
 ################################################################################################################
 FILE=/usr/local/tomcat/webapps/hello-world-web-app.war
-DATE=`date +%b-%d-%y`
+DATE=`date +%Y%m%d_%T`
 if [ -f "$FILE" ]; then
     echo "$FILE exists"
-    mv "$FILE$DATE" 
+    mv "$FILE_$DATE" 
 else
     cp -rf /opt/codedeploy-agent/deployment-root/*/*/deployment-archive/target/*.war /usr/local/tomcat/webapps/
 fi
