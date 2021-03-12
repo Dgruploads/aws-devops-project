147f79d84d04e1bb2431f335f5dfe4db01c03798
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2021-03-12 19:15:17 +0530

    Update install_tomcat.sh

--- scripts/install_tomcat.sh
+++ scripts/install_tomcat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /tmp
-yum install java -y
-wget https://downloads.apache.org/tomcat/tomcat-8/v8.5.64/bin/apache-tomcat-8.5.64.tar.gz
-tar -xzf apache-tomcat-8.5.64.tar.gz
-cp -R apache-tomcat-8.5.64 /usr/local/
-mv /usr/local/apache-tomcat-8.5.64 /usr/local/tomcat/
+#cd /tmp
+#yum install java -y
+#wget https://downloads.apache.org/tomcat/tomcat-8/v8.5.64/bin/apache-tomcat-8.5.64.tar.gz
+#tar -xzf apache-tomcat-8.5.64.tar.gz
+#cp -R apache-tomcat-8.5.64 /usr/local/
+#mv /usr/local/apache-tomcat-8.5.64 /usr/local/tomcat/
