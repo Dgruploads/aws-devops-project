25f4767714861ea4ad1c3b6bf289d7b7d857b261
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2021-03-12 19:54:26 +0530

    Update copy.sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,4 +2,10 @@
 ################################################################################################################
 # Please note this is an example copy path. You will need to change this according to the path on  your server.#
 ################################################################################################################
-yes | sudo cp -rf /opt/codedeploy-agent/deployment-root/*/*/deployment-archive/target/*.war /usr/local/tomcat/webapps/
+FILE=/usr/local/tomcat/webapps/hello-world-web-app.war
+DATE=`date +%b-%d-%`
+if [ -f "$FILE" ]; then
+    echo "$FILE exists"
+    mv "$FILE$DATE" 
+else
+    cp -rf /opt/codedeploy-agent/deployment-root/*/*/deployment-archive/target/*.war /usr/local/tomcat/webapps/
