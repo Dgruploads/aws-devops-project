5d6af6afcd81ec2991ca78c0f79d811f2f9f0eac
root <root@ip-172-31-23-112.ec2.internal>
2021-03-13 13:17:21 +0000

    test

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,11 +2,4 @@
 ################################################################################################################
 # Please note this is an example copy path. You will need to change this according to the path on  your server.#
 ################################################################################################################
-FILE=/usr/local/tomcat/webapps/hello-world-web-app.war
-DATE=`date +%Y%m%d_%T`
-if [ -f "$FILE" ]; then
-    echo "$FILE exists"
-    mv "$FILE" "$FILE$DATE"
-else
-    cp -rf /opt/codedeploy-agent/deployment-root/*/*/deployment-archive/target/*.war /usr/local/tomcat/webapps/
-fi
+cp -rf /opt/codedeploy-agent/deployment-root/*/*/deployment-archive/target/*.war /usr/local/tomcat/webapps/
--- scripts/install_tomcat.sh
+++ scripts/install_tomcat.sh
@@ -5,3 +5,15 @@
 #tar -xzf apache-tomcat-8.5.64.tar.gz
 #cp -R apache-tomcat-8.5.64 /usr/local/
 #mv /usr/local/apache-tomcat-8.5.64 /usr/local/tomcat/
+PACKAGE=/usr/local/tomcat/webapps/hello-world-web-app.war
+FILE=/usr/local/tomcat/webapps/hello-world-web-app
+DATE=`date +%Y%m%d_%T`
+if [ -f "$PACKAGE" ]; then
+    echo "$PACKAGE exists"
+    mv "$PACKAGE" "$PACKAGE$DATE"
+fi
+
+if [ -f "$FILE" ]; then
+   echo "$FILE exists and will be deleted"
+   rm -rf "$FILE"
+fi
