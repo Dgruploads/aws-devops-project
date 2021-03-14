4c94cabfe30abf03f07e16871314277646cd7249
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2021-03-14 16:20:30 +0530

    Update copy.sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,4 +2,6 @@
 ################################################################################################################
 # Please note this is an example copy path. You will need to change this according to the path on  your server.#
 ################################################################################################################
-cp -rf /opt/codedeploy-agent/deployment-root/*/*/deployment-archive/target/*.war /usr/local/tomcat/webapps/
+ls /opt/codedeploy-agent/deployment-root/*/ > /tmp/deployment.txt
+deployment=$(sed -n '/d-/p' /tmp/deployment.txt | head -1)
+cp -rf /opt/codedeploy-agent/deployment-root/*/$deployment/deployment-archive/target/*.war /usr/local/tomcat/webapps/
